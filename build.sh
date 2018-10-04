7269a04be7558eaf0f52ef7c2892f7c330335e48
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-04 09:21:32 +0800

    add user application base directory

--- build.sh
+++ build.sh
@@ -8,4 +8,8 @@ if [[ -z "${FANO_DIR}" ]]; then
 export FANO_DIR="fano"
 fi
 
-fpc @fpc.cfg @build.cfg app/app.pas
+if [[ -z "${USER_APP_DIR}" ]]; then
+export USER_APP_DIR="app"
+fi
+
+fpc @fpc.cfg @build.cfg ${USER_APP_DIR}/app.pas
