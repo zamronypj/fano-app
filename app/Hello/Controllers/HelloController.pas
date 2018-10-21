b0e9c7200acbb3760f83ac22b5426c821a13b7e7
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-21 08:44:47 +0800

    implement placeholder

--- app/Hello/Controllers/HelloController.pas
+++ app/Hello/Controllers/HelloController.pas
@@ -5,13 +5,39 @@ interface
 uses
 
     DependencyIntf,
+    RequestIntf,
+    ResponseIntf,
     ControllerImpl;
 
 type
 
     THelloController = class(TController, IDependency)
+    public
+        function handleRequest(
+            const request : IRequest;
+            const response : IResponse
+        ) : IResponse; override;
     end;
 
 implementation
+
+uses
+
+    PlaceholderTypes;
+
+    function THelloController.handleRequest(
+          const request : IRequest;
+          const response : IResponse
+    ) : IResponse;
+    var placeHolders : TArrayOfPlaceholders;
+        i:integer;
+    begin
+        placeHolders := getArgs();
+        for i:=0 to length(placeholders)-1 do
+        begin
+            viewParams.setVar(placeholders[i].phName, placeholders[i].phValue);
+        end;
+        result := inherited handleRequest(request, response);
+    end;
 
 end.
