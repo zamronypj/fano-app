af9b2646e6bb7275d632f71697036e28ca380d59
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-23 07:59:11 +0800

    initial effort to fix memory leak when file log cannot be open

--- app/Hello/Controllers/HelloControllerFactory.pas
+++ app/Hello/Controllers/HelloControllerFactory.pas
@@ -40,38 +40,47 @@ uses
     TemplateParserFactoryImpl,
     TemplateFileViewImpl,
     ViewParametersIntf,
-    LoggerIntf;
+    LoggerIntf,
+    ViewIntf;
 
     function THelloControllerFactory.build(const container : IDependencyContainer) : IDependency;
     var routeMiddlewares : IMiddlewareCollectionAware;
         config : IAppConfiguration;
         viewParams : IViewParameters;
         logger : ILogger;
+        tmplView : IView;
     begin
         routeMiddlewares := container.get('routeMiddlewares') as IMiddlewareCollectionAware;
         config := container.get('config') as IAppConfiguration;
         viewParams := container.get('viewParams') as IViewParameters;
         logger := container.get('logger') as ILogger;
+        tmplView := TTemplateFileView.create(
+            container.get('outputBuffer') as IOutputBuffer,
+            container.get('templateParser') as ITemplateParser,
+            extractFileDir(getCurrentDir()) + '/Templates/Hello/index.html'
+        );
         try
+            try
             result := THelloController.create(
                 routeMiddlewares.getBefore(),
                 routeMiddlewares.getAfter(),
-                TTemplateFileView.create(
-                    container.get('outputBuffer') as IOutputBuffer,
-                    container.get('templateParser') as ITemplateParser,
-                    extractFileDir(getCurrentDir()) + '/Templates/Hello/index.html'
-                ),
+                tmplView,
                 viewParams
                     .setVar('baseUrl', config.getString('baseUrl'))
                     .setVar('name', 'Fano')
                     .setVar('appName', config.getString('appName')),
                 logger
             );
+            except
+                result :=nil;
+                raise;
+            end;
         finally
             routeMiddlewares := nil;
             config := nil;
             viewParams := nil;
             logger := nil;
+            tmplView := nil;
         end;
     end;
 end.
