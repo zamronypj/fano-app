0311ef5f10a41860703aec6c768265ec7f8419a5
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-27 08:50:14 +0800

    reorganize directory and add controller to handle POST request

---
+++ app/App/Hi/Controllers/HiController.pas
@@ -0,0 +1,46 @@
+(*!------------------------------------------------------------
+ * Fano Web Framework Skeleton Application (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano-app
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano-app/blob/master/LICENSE (GPL 2.0)
+ *------------------------------------------------------------- *)
+unit HiController;
+
+interface
+
+uses
+
+    DependencyIntf,
+    RequestIntf,
+    ResponseIntf,
+    RouteHandlerImpl;
+
+type
+
+    THiController = class(TRouteHandler, IDependency)
+    public
+        function handleRequest(
+            const request : IRequest;
+            const response : IResponse
+        ) : IResponse; override;
+    end;
+
+implementation
+
+uses
+
+    JsonResponseImpl;
+
+    function THiController.handleRequest(
+          const request : IRequest;
+          const response : IResponse
+    ) : IResponse;
+    var strName : string;
+    begin
+        strName := request.getParsedBodyParam('name');
+        strName := request.getParsedBodyParam('name');
+        result := TJsonResponse.create(response.headers(), '{"name":"'+ strName +'"}');
+    end;
+
+end.
